d68691e75ffede6aa879e6338bebc2128afaee8b
Davi <davigabtomaz@gmail.com>
2023-03-17 10:45:33 -0300

    Passo 8 feito

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework.serializers import ModelSerializer
 
-from livraria.models import Categoria
+from livraria.models import Categoria, Editora, Livro
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = '__all__'
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = '__all__'
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+
+class LivroDetalheSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
