b5a12e85b0d67a73e79a4225db9a4766cbd4ed3a
Davi <davigabtomaz@gmail.com>
2023-03-17 14:47:07 -0300

    Feito passo 13

--- livraria/views.py
+++ livraria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from livraria.models import Categoria, Editora, Livro
 from livraria.serializers import CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetalheSerializer
@@ -8,6 +9,7 @@ from livraria.serializers import CategoriaSerializer, EditoraSerializer, LivroSe
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+ #  permission_classes = [IsAuthenticated]
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
